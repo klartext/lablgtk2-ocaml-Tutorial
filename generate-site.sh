2a2f125004efe1fb9e6663be1c98a28ed91f5f81
Martin DeMello <mdemello@google.com>
2015-12-19 13:57:26 -0800

    convert to markdown and add a generator script

---
+++ generate-site.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Convert files from markdown to html.
+# Output will be in the generated-html directory.
+
+mkdir generated-html
+rm -rf generated-html/*
+
+for i in markdown/*.md; do
+  pandoc -f markdown -t html $i > generated-html/$(basename $i .md).html
+done
+
+for i in images stylesheet-images; do
+  cp -r markdown/$i generated-html
+done
